08486720c628d8f43e44206249ede2c5e9ea728c
FranckRomer <paco_metal@hotmail.com>
2022-11-26 01:29:12 -0600

    corazon latiendo

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-// import Link from 'next/link'
+import Link from 'next/link'
 import { Header } from '../../components/header/header'
 import stylesHeader from '../../styles/portfolio/header.module.css'
 import styles from '../../styles/portfolio/homeport.module.css'
@@ -20,28 +20,31 @@ const Portfolio = () => {
                 <section className={styles.main}>
                     <div className={styles.welcome}>
                         <div className={styles.info}>
-                            <h1>Hi!, I'm Franck</h1>
-                            
+                            <h1>Hi!, I am Franck </h1>
+
                             <div className={styles.self}>
-                                <h4>"Full-Stack Developer"</h4>
+                                <h4>Full-Stack Developer</h4>
                                 <p> && </p>
-                                <h4>"IoT Developer"</h4>
+                                <h4>IoT Developer</h4>
                             </div>
                         </div>
                         <div>
 
                         </div>
                     </div>
-                    
+
                     <div className={styles.corazon}>
                         <h1>I love programming</h1>
-                        <Image
-                            src={"/icon/corazon.png"}
-                            alt='User profile picture'
-                            width={120}
-                            height={120}
-                        />
-                        
+                        <Link href={"/portfolio/contact"} className={styles.animation}>
+                            <Image
+                                src={"/icon/corazon.png"}
+                                alt='User profile picture'
+                                width={100}
+                                height={100}
+                            />
+
+                        </Link>
+                        <p>Contact Me</p>
                     </div>
                 </section>
             </div>
